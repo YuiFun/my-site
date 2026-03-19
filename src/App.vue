499c224eedefd4f9056f78ce31d192316dabcf9c
YuiFun <166853252+YuiFun@users.noreply.github.com>
2026-03-19 16:53:37 +0800

    Update IP fetching API and fix welcome message format

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import { onMounted, ref } from 'vue';
 
   const ipAddress=ref("Loading...")
   const getIP=async function getIP() {
-    const response = await fetch('https://api.ipify.org?format=json');
+    const response = await fetch('https://ipapi.co/json/');
     const data = await response.json();
     
     ipAddress.value = data.ip;
@@ -15,7 +15,7 @@ onMounted(() => {
 </script>
 
 <template>
-  <p id="userIP">{{ "Welcome："+ipAddress }}</p>
+  <p id="userIP">{{ "Welcome:"+ipAddress }}</p>
 </template>
 
 <style scoped></style>
