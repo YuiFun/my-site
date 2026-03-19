61d993afc7cfa52757a48968bb8dc344b6811bc9
yuifun <783603107@qq.com>
2026-03-19 15:30:02 +0800

    my-site commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+
+  const ipAddress=ref("Loading...")
+  const getIP=async function getIP() {
+    const response = await fetch('https://api.ipify.org?format=json');
+    const data = await response.json();
+    
+    ipAddress.value = data.ip;
+  }
+onMounted(() => {
+  getIP();
+});
+
+</script>
+
+<template>
+  <p id="userIP">{{ "Welcome"+ipAddress }}</p>
+</template>
+
+<style scoped></style>
